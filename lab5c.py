80f681bdb0b88f7e22a72459005ba9759c3f9e53
Rafien Mohammed <rafinsk974@gmail.com>
2024-11-07 20:30:54 -0500

    Completed lab5c

---
+++ lab5c.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Author ID: 101775229
+
+def add(number1, number2):
+    try:
+        return int(number1) + int(number2)
+    except (ValueError, TypeError):
+        return "error: could not add numbers"
+
+def read_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            return file.readlines()
+    except IOError:
+        return "error: could not read file"
+
+if __name__ == '__main__':
+    print(add(10, 5))                        
+    print(add('10', 5))                      
+    print(add('abc', 5))
+    print(read_file('seneca2.txt'))
+    print(read_file('file10000.txt'))
+
